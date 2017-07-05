ed110bc6abc0fe0fb0a0bc50be50af2f6810672d
beginningcst <957223506@qq.com>
2017-07-05 17:09:17 +0800

    Signed-off-by: beginningcst <957223506@qq.com>

---
+++ app/src/main/java/com/cstlmj/data/Data.java
@@ -0,0 +1,8 @@
+package com.cstlmj.data;
+
+/**
+ * Created by chensentao on 2017/7/4.
+ * 此类作为接口集同一获取数据信息
+ */
+public class Data {
+}
---
+++ app/src/main/java/com/cstlmj/data/XingZuoData.java
@@ -0,0 +1,11 @@
+package com.cstlmj.data;
+
+/**
+ * Created by chensentao on 2017/7/4.
+ */
+public class XingZuoData {
+    public int id;//星座id
+    public String name;//星座名称
+    public String date;
+    public String pic;
+}
